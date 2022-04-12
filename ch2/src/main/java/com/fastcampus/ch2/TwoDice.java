25c09ad5ae1b4339b0b90f98cdce281b4e6653ae
Renee <gracekang61@naver.com>
2022-04-12 23:54:30 +0900

    requestMessage

--- ch2/src/main/java/com/fastcampus/ch2/TwoDice.java
+++ ch2/src/main/java/com/fastcampus/ch2/TwoDice.java
@@ -8,25 +8,51 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller// ctrl + shift + o : 자동 import
-public class TwoDice {
+@Controller
+public class TwoDice{
 	@RequestMapping("/rollDice")
-	public void main(HttpServletResponse response) throws IOException {
+	private void main(HttpServletResponse response) throws IOException {
+		
 		int idx1 = (int)(Math.random()*6)+1;
 		int idx2 = (int)(Math.random()*6)+1;
-		
+				
 		response.setContentType("text/html");
 		response.setCharacterEncoding("utf-8");
-		
 		PrintWriter out = response.getWriter();
-		
 		out.println("<html>");
 		out.println("<head>");
 		out.println("</head>");
 		out.println("<body>");
-		out.println("<img src='resources/img/dice"+idx1+".jpg'>");
-		out.println("<img src='resources/img/dice"+idx2+".jpg'>");
+		out.println("<img src='img/dice"+idx1+".jpg'>");
+		out.println("<img src='img/dice"+idx2+".jpg'>");
 		out.println("</body>");
 		out.println("</html>");
 	}
 }
+
+
+
+
+//
+//@Controller// ctrl + shift + o : 자동 import
+//public class TwoDice {
+//	@RequestMapping("/rollDice")
+//	public void main(HttpServletResponse response) throws IOException {
+//		int idx1 = (int)(Math.random()*6)+1;
+//		int idx2 = (int)(Math.random()*6)+1;
+//		
+//		response.setContentType("text/html");
+//		response.setCharacterEncoding("utf-8");
+//		
+//		PrintWriter out = response.getWriter();
+//		
+//		out.println("<html>");
+//		out.println("<head>");
+//		out.println("</head>");
+//		out.println("<body>");
+//		out.println("<img src='resources/img/dice"+idx1+".jpg'>");
+//		out.println("<img src='resources/img/dice"+idx2+".jpg'>");
+//		out.println("</body>");
+//		out.println("</html>");
+//	}
+//}
--- ch2/src/main/java/com/fastcampus/ch2/YoilTeller.java
+++ ch2/src/main/java/com/fastcampus/ch2/YoilTeller.java
@@ -10,42 +10,11 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-////년원일을 입력하면 요일을 알려주는 프로그램
-//public class YoilTeller {
-//	public static void main(String[] args) {
-//		
-//		//1.입력
-//		String year = args[0];
-//		String month = args[0];
-//		String day = args[2];
-//		
-//		int yyyy= Integer.parseInt(year);
-//		int mm= Integer.parseInt(month);
-//		int dd= Integer.parseInt(day);
-//		
-//		//2.작업
-//		Calendar cal = Calendar.getInstance();
-//		cal.set(yyyy, mm-1,dd);
-//		
-//		int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK);//1:일요일,2:월요일...
-//		char yoil = " 일월화수목금토".charAt(dayOfWeek);
-//		
-//		//3. 출력
-//		System.out.println(year + "년"+month+"월"+day+"일은");
-//		System.out.println(yoil+"요일입니다.");
-//		
-//		
-//	}
-//}
-
-
 //년원일을 입력하면 요일을 알려주는 프로그램
 @Controller
-public class YoilTeller {//http://localhost:8080/ch2/getYoil?year=2021&month=10&day=1
-	
-	
+public class YoilTeller {
 	@RequestMapping("/getYoil")
-	public void main(HttpServletRequest request,HttpServletResponse response) throws IOException{
+	public static void main(HttpServletRequest request, HttpServletResponse response) throws IOException{
 		
 		//1.입력
 		String year = request.getParameter("year");
@@ -56,6 +25,7 @@ public class YoilTeller {//http://localhost:8080/ch2/getYoil?year=2021&month=10&
 		int mm= Integer.parseInt(month);
 		int dd= Integer.parseInt(day);
 		
+		//2.작업 (요일계산)
 		Calendar cal = Calendar.getInstance();
 		cal.set(yyyy, mm-1,dd);
 		
@@ -63,16 +33,20 @@ public class YoilTeller {//http://localhost:8080/ch2/getYoil?year=2021&month=10&
 		char yoil = " 일월화수목금토".charAt(dayOfWeek);
 		
 		//3. 출력
-		response.setContentType("text/html");//텍스트 보낸다고 알려주고
-		response.setCharacterEncoding("utf-8");//인코딩 알려주고
-		
-		PrintWriter out = response.getWriter(); //response객체에서 브라우저로의 출력 스트림을 얻는다.
-		
+		response.setContentType("text/html");
+		response.setCharacterEncoding("utf-8");
+		PrintWriter out = response.getWriter();
 		out.println(year + "년"+month+"월"+day+"일은");
 		out.println(yoil+"요일입니다.");
-//		System.out.println(year + "년"+month+"월"+day+"일은");
-//		System.out.println(yoil+"요일입니다.");
 		
 		
 	}
 }
+
+
+
+
+
+
+
+
