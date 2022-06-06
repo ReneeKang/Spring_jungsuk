e76830ec6840deadbefbc32e00c3d625e5888a58
Renee <gracekang61@naver.com>
2022-06-07 00:19:46 +0900

    make board

---
+++ ch4/src/main/java/com/fastcampus/ch4/controller/BoardController.java
@@ -0,0 +1,74 @@
+package com.fastcampus.ch4.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import com.fastcampus.ch4.dao.BoardDao;
+import com.fastcampus.ch4.domain.BoardDto;
+import com.fastcampus.ch4.domain.PageHandler;
+import com.fastcampus.ch4.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/board")
+public class BoardController {
+
+    @Autowired
+    BoardService boardService;
+
+    @GetMapping("/read")
+    public String read(Integer bno, Model m){ //게시물번호받자
+        try {
+            BoardDto boardDto = boardService.read(bno);
+//            m.addAttribute("boardDto",boardDto); //아래문장과 동일
+            m.addAttribute(boardDto); //이름생략시 타입첫글자를 소문하로한걸 이름으로 저장
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return "board";
+    }
+
+    @GetMapping("/list")
+    public String list(Integer page, Integer pageSize, Model m, HttpServletRequest request) {
+        if(!loginCheck(request))
+            return "redirect:/login/login?toURL="+request.getRequestURL();  // 로그인을 안했으면 로그인 화면으로 이동
+
+        if(page==null) page=1;
+        if(pageSize==null) pageSize=10;
+
+        try {
+            int totalCnt = boardService.getCount();
+            PageHandler pageHandler = new PageHandler(totalCnt,page,pageSize);
+
+            Map map = new HashMap();
+            map.put("offset",(page-1)*pageSize);
+            map.put("pageSize", pageSize);
+
+            List<BoardDto> list = boardService.getPage(map);
+            m.addAttribute("list",list);
+            m.addAttribute("ph",pageHandler);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return "boardList"; // 로그인을 한 상태이면, 게시판 화면으로 이동
+    }
+
+    private boolean loginCheck(HttpServletRequest request) {
+        // 1. 세션을 얻어서
+        HttpSession session = request.getSession();
+        // 2. 세션에 id가 있는지 확인, 있으면 true를 반환
+        return session.getAttribute("id")!=null;
+    }
+}
